d3282fb9ce0cc83d095a02f4e270576f1c31b261
금젤 <joimh7@gmail.com>
2024-04-20 15:01:57 +0900

    - Dialog 창 만들기 - context 의미 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -12,7 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   var a = 1;
   var names = ['Steve', 'John', 'Robert', 'Doe', 'Smith'];
   var likes = [0, 0, 0, 0, 0];
@@ -20,70 +19,142 @@ class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {
-              print(a);
-              setState(() {
-                a++;
-              });
-            },
-            child: Text(a.toString()),
-          ),
-          appBar: AppBar(
-            title: Text(
-                'My App',
-                style: TextStyle(
-                  color: Colors.white,
-                )
+    return Scaffold(
+      floatingActionButton: DialogConfirm(),
+      appBar: AppBar(
+        title: Text('My App',
+            style: TextStyle(
+              color: Colors.white,
+            )),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return ListTile(
+              title: Text(names[index]),
+              // leading: Icon(Icons.account_circle),
+              leading: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(likes[index].toString()),
+                  Icon(Icons.account_circle),
+                ],
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    likes[index]++;
+                  });
+                },
+                child: Text('좋아요'),
+              ));
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.phone),
+              onPressed: () {},
             ),
-            backgroundColor: Colors.blue,
-          ),
-          body: ListView.builder(
-            itemCount: 5,
-            itemBuilder: (context, index) {
-              return ListTile(
-                  title: Text(names[index]),
-                  // leading: Icon(Icons.account_circle),
-                  leading: Row(
-                    mainAxisSize: MainAxisSize.min,
+            IconButton(
+              icon: Icon(Icons.chat),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.contact_page),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DialogConfirm extends StatelessWidget {
+  const DialogConfirm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Builder(builder: (context) {
+      return FloatingActionButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return Dialog(
+                  child: Column(
                     children: <Widget>[
-                      Text(likes[index].toString()),
-                      Icon(Icons.account_circle),
+                      // Header
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children:const <Widget>[
+                              Text('Contact'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      // input field
+                      SizedBox(
+                        // width percentage
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: TextField(
+                          // margin
+                          decoration: InputDecoration(
+                            // border == only underline
+                            border: UnderlineInputBorder(),
+                            // margin
+                            contentPadding: const EdgeInsets.all(10.0),
+                            labelText: 'phone number',
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              ElevatedButton(
+                                // shape == square
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Cancel'),
+                              ),
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('OK'),
+                              ),
+                            ],
+                          ),
+                        )
+                      )
+
                     ],
                   ),
-                  trailing: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        likes[index]++;
-                      });
-                    },
-                    child: Text('좋아요'),
-                  )
-              );
-            },
-          ),
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.phone),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.chat),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.contact_page),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ),
-        )
-    );
+                );
+              });
+        },
+        child: Text('버튼'),
+      );
+    });
   }
 }
